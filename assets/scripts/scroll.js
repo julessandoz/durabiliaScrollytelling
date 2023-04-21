f4c4c2ffc7d2613b3e34e9f79cdc6eef1def162c
dk-xy <92148260+dk-xy@users.noreply.github.com>
2023-04-21 14:34:01 +0200

    nuages et avatar

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,3 +1,6 @@
+// import slide1.js script
+
+
 console.log('onScroll')
 
 const showMarkers = true;
@@ -14,20 +17,7 @@ gsap.registerPlugin(MotionPathPlugin);
 
 
 
-// SECTION 1------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-// ANIMATION ICONE DURABILIA-----------------------------------------------------------
-
-
-
-
-let tl = gsap.timeline({});
-let tlScroll = gsap.timeline({});
-let tlAvatar = gsap.timeline({});
-tlScroll.add('start');
-tlAvatar.add('anime');
 
 let floatIcon = gsap.to(".durabilia-icon", {
   y: -20,
@@ -38,50 +28,21 @@ let floatIcon = gsap.to(".durabilia-icon", {
   })
 
 
-// from opacity 0 to 1 and scale 1 to 0
-tlScroll.to(".durabilia-icon", {
-    scrollTrigger: {
-        trigger: "#section-1",  
-        start: "top+=5 top",
-        end: "+=10",
-        scrub: 1,
-        // markers: true,
-        // pin content with lots of pinSpacing
-        pin: true,
-        pinSpacing: true,
-        onEnter: () => floatIcon.pause(),
 
-        //toggleActions: "play reverse play reverse"
-    },
-    // x:-180,
-    opacity: 1,
-    scale: 0.5,
+  let floatAvatar = gsap.to(".avatar", {
+    y: "-=20",
     duration: 1,
-    ease: "power1.inOut"
-}, 'start');
+    ease: "power1.inOut",
+    yoyo: true,
+    repeat: -1,
+    })
 
+    let tlAvatar = gsap.timeline({
+      // floatAvatar on enter
+      onEnter: () => floatAvatar.play(),
+    });
 
-        // ANIMATION TEXT--------------------------
-
-
-tlScroll.to(".logo-text", {
-  scrollTrigger: {
-      trigger: "#section-1",  
-      start: "top+=15 top",
-      end: "+=150",
-      scrub: 1,
-      // markers: true,
-      // pin content with lots of pinSpacing
-      pin: true,
-      pinSpacing: true,
-
-      //toggleActions: "play reverse play reverse"
-  },
-  // x:-180,
-opacity:1,
-  ease: "power1.inOut"
-}, 'start');
-
+    tlAvatar.add('anime');
 // FIN ANIM DURABILIA---------------------------------------------------
 
 
@@ -91,7 +52,10 @@ opacity:1,
 let draw = SVG().addTo('.avatarContainer');
 
 // make draw full with and height, position absolute and z-index 2
-draw.size('1000', '600').style('position: absolute; z-index: 2;');
+// draw.size('1920', '100%').style('position: absolute; z-index: 2;');
+
+// draw at full size
+draw.size('100%', '100%').style('position: absolute; z-index: 2;');
 
 // create a triangle
 let triangle = draw.polygon('0,50 25,0 50,50').fill('#FFEBC3').stroke({ width: 3, color: '#EED4A0' })
@@ -122,32 +86,13 @@ triangle.back();
 
 // create an arc path with id "motionPath" at avatarGroup position
 let motionPath = draw.path("M 50 50 A 50 50 0 0 1 150 50").id("motionPath").move(avatarGroup.x(), avatarGroup.y()).fill('none').stroke('none');
+
+
 avatarGroup.opacity(0);
 
 // ANIMATION AVATAR-------------------------------
 
 // add to tlScroll an animation to make the avatarGroup appear and move
-tlAvatar.to(avatarGroup.node, {
-  scrollTrigger: {
-      trigger: "#section-1",
-      start: "top+=165 top",
-      end:"top+=160 top",
-
-      scrub: 1,
-      // markers: true,
-      // pin content with lots of pinSpacing
-      pin: true,
-      pinSpacing: true,
-      //toggleActions: "play reverse play reverse"
-  },
-// move x and y randomly, rotate slightly, and scale up
-
-
-  duration: 1,
-  opacity: 1,
-  scale: 1.2,
-  ease: "power1.inOut"
-}, 'anime');
 
 
 // define the path
@@ -162,6 +107,20 @@ const path = {
 
 let motionPath2 = draw.path('M50,100 C50,150 150,150 150,100 S250,50 350,100').id('motionPath2').move(avatarGroup.x(), avatarGroup.y()).fill('none').stroke('none');
 
+
+// // get the end point of the existing path
+const pathLength = motionPath2.length();
+const end = motionPath2.pointAt(pathLength);
+
+// define the new curved path
+const curvedPath = draw.path(`M${end.x},${end.y} C${end.x + 100},${end.y + 50} ${end.x + 200},${end.y - 50} ${end.x + 300},${end.y}`).id('motionPath3').fill('none').stroke('none');
+
+// animate the avatar along the new curved path
+
+
+// another path
+
+
 const path2 = {
   path: "#motionPath2",
   align: "#motionPath2",
@@ -170,53 +129,89 @@ const path2 = {
   end: 1
 };
 
+const path3 = {
+  path: "#motionPath3",
+  align: "#motionPath3",
+  autoRotate: true,
+  start: 0,
+  end: 1
+};
+
+
 
 
 tlAvatar.to(avatarGroup.node, {
   scrollTrigger: {
-      trigger: "#section-1",
-      start: "top+=260 top",
-      end:"top+=290 top",
-      scrub: 1,
-      // markers: true,
-      // pin content with lots of pinSpacing
-      // pin: true,
-      // pinSpacing: true,
-      toggleActions: "play reverse play reverse"
+    trigger: "#section-1",
+    start: "top+=260 top",
+    end: "top+=290 top",
+    scrub: 1,
+    toggleActions: "play reverse play reverse"
   },
   duration: 3,
   opacity: 1,
   scale: 1.2,
   ease: "power1.inOut",
-  // add path and rotateX
-  motionPath: path2,
-
-}, 'anime');
+  motionPath: path2
+}, "anime");
 
 tlAvatar.to(avatarGroup.node, {
   scrollTrigger: {
-      trigger: "#section-1",
-      start: "top+=320 top",
-      end:"top+=380 top",
-      scrub: 1,
-      // markers: true,
-      // pin content with lots of pinSpacing
-      // pin: true,
-      // pinSpacing: true,
-      toggleActions: "play reverse play reverse",
-
-
+    trigger: "#section-2",
+    start: "top+=300 top",
+    end: "top+=400 top",
+    scrub: 1,
+    toggleActions: "play reverse play reverse"
   },
-  duration: 6,
-
+  duration: 3,
+  opacity: 1,
+  scaleX: -1.2,
   ease: "power1.inOut",
-  // add path and rotateX
-y: 200,
+  motionPath: path3
+}, "anime+=1");
 
-}, 'anime');
+
+tlAvatar.to(avatarGroup.node, {
+  scrollTrigger: {
+    trigger: "#section-2",
+    start: "top+=800 top",
+    end: "top+=900 top",
+    scrub: 1,
+    toggleActions: "play reverse play reverse"
+  },
+  x:"+=600",
+  scaleX: 1.2,
+  ease: "power1.inOut",
+}, "anime+=2");
+
+
+tlAvatar.to(avatarGroup.node, {
+  scrollTrigger: {
+    trigger: "#section-9",
+    start: "top top",
+    end: "top bottom",
+    scrub: 1,
+    toggleActions: "play reverse play reverse"
+  },
+  y:"+=700",
+  scaleX: -1.2,
+  ease: "power1.inOut",
+}, "anime+=3");
+
+
 
 
 //FIN ANIM AVATAR--------------------------------------------
+
+// SECTION 1------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// ANIMATION ICONE DURABILIA-----------------------------------------------------------
+
+let tl = gsap.timeline({});
+
+
 
 
 
@@ -282,14 +277,19 @@ bgTl.add('play');
 
 // CORRIGER ICI LA WIDTH POUR LE TEXT
 
-let boxWidth = 520,
+let boxWidth = 700,
     totalWidth = boxWidth * 7,  //  * n of boxes
     no01 = document.querySelectorAll("#no01 .box"),
     no02 = document.querySelectorAll("#no02 .box"),
     no03 = document.querySelectorAll("#no03 .box"),
+    no04 = document.querySelectorAll("#no04 .box"),
+    no05 = document.querySelectorAll("#no05 .box"),
+
     dirFromLeft = "+=" + totalWidth,
     dirFromRight = "-=" + totalWidth;
 
+// move #no05 to bottom
+gsap.set("#no05", {y: 600});
 let mod = gsap.utils.wrap(0, totalWidth);
 
 function marquee(which, time, direction){
@@ -315,15 +315,60 @@ let master = gsap.timeline({})
 .add(marquee(no01, 15, dirFromLeft))
 .add(marquee(no02, 20, dirFromRight),0)
 .add(marquee(no03, 25, dirFromLeft),0)
+.add(marquee(no04, 25, dirFromLeft),0)
+.add(marquee(no05, 25, dirFromLeft),0)
 // =============================
 
 var action = gsap.timeline({defaults: {duration: 1, ease:'none'}})
 
 
 
+// select cloudContainer
+let cloudContainer = document.querySelector('.cloudCanvas');
+
+// append a svg canvas with multiple white circles svg.js
+let drawClouds = SVG().addTo(cloudContainer).size('100%', '100%');
+
+// create a group
+
+// create circles with random position and size nested in the group
+for (let i = 0; i < 20; i++) {
+  let cloudGroup = drawClouds.group();
+  // create circles with random position and size nested in the group
+  for (let i = 0; i < 20; i++) {
+  let cloud = cloudGroup.circle(100).fill('#c0c3c4').move(Math.random() * 200, Math.random() * 100).radius(Math.random() * 140);
+  cloudGroup.add(cloud);
+  // add class to group
+  cloudGroup.addClass('cloudGroup');
+  // add class with index
+  cloudGroup.addClass('cloudGroup-' + i);
+// add to group
+  }
+  // move cloudgroup x
+  cloudGroup.y(Math.random() * 1000);
+  // move x to -100
+  cloudGroup.x(Math.random() * 20);
+  // cloudGroup.x(Math.random() * 200);
 
 
-tl2.add('start');
+// place randomly on left or right
+if (Math.random() > 0.5) {
+  cloudGroup.cx(Math.random() * 1500);
+} else {
+  cloudGroup.cx(Math.random() * -1500);
+  
+
+}
+}
+
+
+
+
+// foreach in cloudGroup to move them around the canvas
+
+
+
+
 
 
 tl2.to('.text-1', {
@@ -331,6 +376,7 @@ tl2.to('.text-1', {
   opacity: 1,
   scale: 1,
   duration: 1,
+
   ease: "power1.inOut"
 }, 'start')
 
@@ -338,24 +384,63 @@ tl2.to('.text-1', {
 .to('#no01', {
   opacity: 1,
   duration: 1,
+
   ease: "power1.inOut"
   }, 'start')
 
+ 
 
 // add another animation after this one to fade it out
+.from('#no01', {
+  y:300,
+  scale: 1.2,
+  duration: 1,
+  ease: "power1.inOut",
+
+  }, 'start')
 tl2.to('.text-1', {
   x:0,
   opacity: 0,
   scale: 1,
   duration: 1,
+
   ease: "power1.inOut"
 }, 'start+=1')
+
 
 .to('#no01', {
   opacity: 0,
   duration: 1,
+
   ease: "power1.inOut"
   }, 'start+=1')
+
+  .from('.cloudGroup', {
+    x:0,
+    opacity: 0,
+    scale: 1,
+    duration: 1,
+
+    ease: "power1.inOut"
+    }, 'start+=1'
+    )
+
+  .to('.cloudCanvas', {
+    opacity: 1,
+    duration: 1,
+
+    ease: "power1.inOut"
+    }, 'start+=1'
+  )
+  .to('.cloudGroup', {
+    x: 2000,
+    duration: 1,
+    ease: "power1.inOut",
+    stagger: 0.1,
+    opacity: 0,
+
+    }, 'start+=2'
+  )
 
 // FIN SLIDE 1------------------------
 
@@ -369,13 +454,20 @@ tl2.to('.text-2', {
   scale: 1,
   duration: 1,
   ease: "power1.inOut"
-}, 'start+=2')
+}, 'start+=3')
+
+.from('#no02', {
+  y:300,
+  scale: 1.2,
+  duration: 1,
+  ease: "power1.inOut"
+  }, 'start+=3')
 
 .to('#no02', {
   opacity: 1,
   duration: 1,
   ease: "power1.inOut"
-  }, 'start+=2')
+  }, 'start+=3')
 
 
 tl2.to('.text-2', {
@@ -386,12 +478,12 @@ tl2.to('.text-2', {
 
   ease: "power1.inOut"
   
-}, 'start+=3')
+}, 'start+=4')
 .to('#no02', {
   opacity: 0,
   duration: 1,
   ease: "power1.inOut"
-  }, 'start+=3');
+  }, 'start+=4');
 
 // FIN SLIDE 2------------------------
 // SLIDE 3------------------------
@@ -402,12 +494,12 @@ tl2.to('.text-3', {
   scale: 1,
   duration: 1,
   ease: "power1.inOut"
-}, 'start+=4')
+}, 'start+=5')
 .to('#no03', {
   opacity: 1  ,
   duration: 1,
   ease: "power1.inOut"
-  }, 'start+=4')
+  }, 'start+=5')
 
 
 tl2.to('.text-3', {
@@ -418,12 +510,30 @@ tl2.to('.text-3', {
 
   ease: "power1.inOut"
   
-}, 'start+=5')
+}, 'start+=6')
+
 .to('#no03', {
   opacity: 0,
   duration: 1,
   ease: "power1.inOut"
-  }, 'start+=5');
+  }, 'start+=6');
+
+// SLIDE 4------------------------
+// element-earth at center of screen
+tl2.to('.element-earth', {
+  x:500,
+  opacity: 1,
+  scale: 1.2,
+  duration: 1,
+  zIndex:0,
+  ease: "power1.inOut"
+}, 'start+=7')
+
+.to(['#no04', '#no05'], {
+  opacity: 1,
+}, 'start+=7')
+
+// hide avatar
 
 
 
@@ -602,6 +712,62 @@ drawSquare(x, y, width, width, color);
 
 
 
+
+
+
+
+// function animateText(selector, bgSelector, start) {
+//   const tl = gsap.timeline({paused:true});
+
+//   tl.to(selector, {
+//     x:0,
+//     opacity: 1,
+//     scale: 1,
+//     duration: 1,
+//     ease: "power1.inOut"
+//   }, start)
+
+//   // Fade in the text-bg
+//   .to(bgSelector, {
+//     opacity: 1,
+//     duration: 1,
+//     ease: "power1.inOut"
+//   }, start)
+
+//   // Add another animation after this one to fade it out
+//   .from(bgSelector, {
+//     y:300,
+//     scale: 1.2,
+//     duration: 1,
+//     ease: "power1.inOut"
+//   }, start)
+
+//   // Fade out the text and text-bg
+//   .to(selector, {
+//     x:0,
+//     opacity: 0,
+//     scale: 1,
+//     duration: 1,
+//     ease: "power1.inOut"
+//   }, `${start}+=1`)
+
+//   .to(bgSelector, {
+//     opacity: 0,
+//     duration: 1,
+//     ease: "power1.inOut"
+//   }, `${start}+=1`);
+
+//   return tl;
+// }
+
+
+
+// tl2.add('start');
+
+
+// animateText('.text-1', '#no01', 'start').play();
+// animateText('.text-2', '#no02', 'start+=2').play();
+// animateText('.text-3', '#no03', 'start+=4').play();
 
 
 
