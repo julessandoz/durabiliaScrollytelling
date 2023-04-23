1543836fe63bc2fa4164a8b3bd43546a7c31c12b
dk-xy <92148260+dk-xy@users.noreply.github.com>
2023-04-23 20:05:48 +0200

    backgroundText des atouts

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -231,6 +231,7 @@ let boxWidth = 700,
     no08 = document.querySelectorAll("#no08 .box"),
     no09 = document.querySelectorAll("#no09 .box"),
     no10 = document.querySelectorAll("#no10 .box"),
+    no11 = document.querySelectorAll("#no11 .box"),
 
     dirFromLeft = "+=" + totalWidth,
     dirFromRight = "-=" + totalWidth;
@@ -269,6 +270,7 @@ let master = gsap.timeline({})
 .add(marquee(no08, 25, dirFromLeft),0)
 .add(marquee(no09, 25, dirFromLeft),0)
 .add(marquee(no10, 25, dirFromLeft),0)
+.add(marquee(no11, 25, dirFromLeft),0)
 var action = gsap.timeline({defaults: {duration: 1, ease:'none'}})
 // =============================
 
@@ -802,7 +804,7 @@ let tlCard9 = gsap.timeline({
   scrollTrigger: {
     trigger: "#section-9",
     start: "top top",
-    end: "+=200%",
+    end: "+=100%",
     scrub: true,
     pin: true,
     pinSpacing: true,
@@ -831,6 +833,10 @@ tlCard9
 
 
 
+// Section 10
+
+
+
 gsap.utils.toArray(".sdg-block").forEach((section, index) => {
 const tl = gsap.timeline({
   scrollTrigger: {
@@ -855,12 +861,28 @@ let tlcard11 = gsap.timeline({
   scrollTrigger: {
     trigger: "#section-10",
     start: "top top",
-    end: "+=200%",
+    end: "+=100%",
     scrub: true,
     pin: true,
     pinSpacing: true,
+  
   }
 });
+
+tlcard11
+.add('start')
+.from('#no11', {
+  scale: 1.2,
+  duration: 1,
+  ease: "power1.inOut"
+}, 'start')
+.to('#no11', {
+  opacity: 1,
+  duration: 1,
+  ease: "power1.inOut"
+}, 'start')
+
+
 
 
 // make a from 0 opacity and -30 x position when reaching #section-9
@@ -953,7 +975,7 @@ atouts.forEach((atout) => {
     setTimeout(() => {
       atoutOverlay.style.display = 'none';
     }, 1000);
-    
+
     
     atoutOverlay.style.zIndex= '1';
   });
@@ -962,368 +984,4 @@ atouts.forEach((atout) => {
 
 
 
-
-
-// gsap.utils.toArray(".section-10-item").forEach((section, index) => {
-//   gsap.to(section, {
-//       scrollTrigger: {
-//           trigger: section,
-//           start: "bottom+=600 center",
-//           end: "+=200",
-//           scrub: 1,
-//           // markers: showMarkers,
-//           //toggleActions: "play reverse play reverse"
-//       },
-//       backgroundColor: "#000",
-//       color: "#fff",
-//       duration: 0.2,
-//       ease: "power1.inOut",
-//       delay: index * 0.1, // Stagger the animations
-//       scale: 1,
-
-//   });
-
-//   //  REVERT TO INITIAL STATE AFTER
-//   gsap.to(section, {
-//       scrollTrigger: {
-//           trigger: section,
-//           start: "bottom+=600 center",
-//           end: "+=350",
-//           scrub: 1,
-//           // markers: showMarkers,
-//           //toggleActions: "play reverse play reverse"
-//       },
-//       backgroundColor: "#ddd",
-//       color:"#000",
-//       duration: 0.2,
-//       opacity: 0.5,
-//       ease: "power1.inOut",
-//       // scale: 0.1 // Scale down the card
-//   });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function animateText(selector, bgSelector, start) {
-//   const tl = gsap.timeline({paused:true});
-
-//   tl.to(selector, {
-//     x:0,
-//     opacity: 1,
-//     scale: 1,
-//     duration: 1,
-//     ease: "power1.inOut"
-//   }, start)
-
-//   // Fade in the text-bg
-//   .to(bgSelector, {
-//     opacity: 1,
-//     duration: 1,
-//     ease: "power1.inOut"
-//   }, start)
-
-//   // Add another animation after this one to fade it out
-//   .from(bgSelector, {
-//     y:300,
-//     scale: 1.2,
-//     duration: 1,
-//     ease: "power1.inOut"
-//   }, start)
-
-//   // Fade out the text and text-bg
-//   .to(selector, {
-//     x:0,
-//     opacity: 0,
-//     scale: 1,
-//     duration: 1,
-//     ease: "power1.inOut"
-//   }, `${start}+=1`)
-
-//   .to(bgSelector, {
-//     opacity: 0,
-//     duration: 1,
-//     ease: "power1.inOut"
-//   }, `${start}+=1`);
-
-//   return tl;
-// }
-
-
-
-// tl2.add('start');
-
-
-// animateText('.text-1', '#no01', 'start').play();
-// animateText('.text-2', '#no02', 'start+=2').play();
-// animateText('.text-3', '#no03', 'start+=4').play();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Zoom sur suisse-----------------
-
-// tlScroll.from(".section-4-item", {
-//     scrollTrigger: {
-//         trigger: "#section-4",
-        
-//          start: "top bottom",
-//          end: "+=200",
-//         scrub: 1,
-//         // markers: showMarkers,
-//         toggleActions: "play reverse play reverse"
-//         },
-//         //opacity: 0,
-//         x: -2000,
-//         duration: 1,
-//         stagger: 0.5,
-//         ease: "power1.inOut"
-//         }, 'start');
-
-
-// // animation logo durabilia-----------------
-
-// tlScroll.from("#logo-durabilia", {
-//     scrollTrigger: {
-//         trigger: "#section-5",
-//         start: "top center",
-//         end: "+=200",
-//         scrub: 1,
-//         // markers: showMarkers,
-//         //toggleActions: "play reverse play reverse"
-//     },
-//     opacity: 0,
-//     duration: 1,
-//     ease: "power1.inOut"
-// }, 'start');
-
-
-
-
-// // section 4--------------------------------------------
-// // Add animations to the timeline
-
-// let tlCard4 = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: "#section-4",
-//         start: "top top",
-//         end: "bottom top",
-//         scrub: true,
-//         // pin: true,
-//         pinSpacing: false
-//       }
-//     }
-//   );
-  
-
-// tlCard4.to(".collab-container", {
-//     opacity: 1,
-//     duration: 0.5
-//   })
-
-
-// //   section 5----------------------------------------------------------
-//   let tlCard5 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: "#section-4",
-//       start: "bottom top",
-//       endTrigger: "#section-6",
-//       end: "top top",
-//       scrub: true,
-//       pin: true,
-//       pinSpacing: false,
-//       onEnter: () => {
-//         tlCard5.to("#logo-durabilia", {
-//           opacity: 1,
-//           scale: 1,
-//           duration: 0.5
-//         });
-//       },
-//       onLeave: () => {
-//         tlCard5.to("#logo-durabilia", {
-//           opacity: 0,
-//           scale: 0,
-//           duration: 0.5
-//         });
-//       }
-//     }
-//   });
-
-
-//         // logo anim---------------
-// // Create a timeline for the logo animation
-// var tlLogo = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: "#section-5",
-//         start: "top top", // Account for the height of the spacer element
-//         end: "bottom top",
-//         scrub: true,
-//         pin: true,
-//         pinSpacing: false
-//     }
-// });
-
-// // Add an animation to the timeline
-// tlLogo.to("#logo-durabilia", {
-//     // opacity: 1,
-//     scale: 1.2,
-//     duration: 0.5,
-//     ease: "power1.inOut"
-// });
-
-
-
-// // section 6 7 8
-
-// // fade in section-6 content on enter and fade out on leave
-// let tlCard6 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: "#section-6",
-//       start: "top top",
-//       end: "center top",
-//       scrub: true,
-//       pin: false,
-//       pinSpacing: false,
-//       markers: showMarkers,
-//     }
-//   });
-
-//   tlCard6.to("#section-6", {
-//     opacity: 0,
-//     duration: 0.5
-
-
-//   })
-
-
-
-//   //  pin section 7 when scrolling
-// let tlCard7 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: "#section-7",
-//       start: "top top",
-//       end: "bottom top",
-//       // scrub: true,
-//       pin: false,
-//       pinSpacing: true
-//     }
-//   });
-
-// // pin section 8 when scrolling
-// let tlCard8 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: "#section-8",
-//       start: "top top",
-//       end: "bottom top",
-//       // scrub: true,
-//       pin: true,
-//       pinSpacing: false
-//     }
-//   });
-
-
-// add animation on section-6
-// let tlCard6 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: "#section-6",
-//       start: "top top",
-//       end: "center top",
-//       scrub: true,
-//       pinSpacing: false,
-//       markers: showMarkers,
-//     }
-//   });
-
-//   tlCard6.fromTo(".community", {opacity: 0}, {
-//     opacity: 1,
-//     duration: 0.5
-//   })
-
-
-
-
-//   // Create another timeline for section-5
-// var tl2 = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: "#section-5",
-//       start: "top top",
-//       end: "bottom top",
-//       scrub: true,
-//       pin: true,
-//       pinSpacing: false
-//     }
-//   });
-  
-//   // Add animations to the timeline
-//   tl2.to("#logo-durabilia", {
-//     opacity: 1,
-//     scale: 1,
-//     duration: 0.5
-//   });
-  
-
-// text reveal sur section 10-----------------
-
-// animate section 10
-// tlScroll.from(".section-10-item", {
-//     scrollTrigger: {
-//         trigger: "#section-10",
-//         start: "top center",
-//         end: "+=200",
-//         scrub: 1,
-//         markers: showMarkers,
-//         //toggleActions: "play reverse play reverse"
-//     },
-//     opacity: 0,
-
-//     duration: 1,
-//     stagger: 0.5,
-//     ease: "power1.inOut"
-// }, 'start');
 
